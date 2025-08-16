844b8149defbf8298d0b3515d921f8a97aeab65a
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-08-16 17:08:49 +0200

    Update screens.dart

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,2 +1,3 @@
 //para facilirar el enrutado, se pone la ruta de todos los de esta carpeta en un fichero con el mismo nombre
 export 'package:flutter_app_turismo_madrid/screens/home_screen.dart';
+export 'package:flutter_app_turismo_madrid/screens/alert_screen.dart';
