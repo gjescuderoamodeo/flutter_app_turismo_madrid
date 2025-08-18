3b3c7acc593520db0afe982845d3ecbf2092e3d6
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2025-08-18 13:03:08 +0200

    Create sitio_datos.dart

---
+++ lib/models/sitio_datos.dart
@@ -0,0 +1,13 @@
+class Sitio {
+  final String nombre;
+  final String ubicacion;
+  final String horario;
+  final String comerCercano;
+
+  Sitio({
+    required this.nombre,
+    required this.ubicacion,
+    required this.horario,
+    required this.comerCercano,
+  });
+}
